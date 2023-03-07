10c8c55c178af018142b5a7ed9868abcb9c97f42
Seth Bangert <seth@sbang.me>
2023-03-07 14:41:16 -0700

    UI Imprmnts. Added function to open tabs from json

--- tabson.js
+++ tabson.js
@@ -1,10 +1,5 @@
-/* saveTabs() is a function that saves the current tabs in the active window to a JSON file. 
-It uses the browser.tabs.query() method to get all tabs in the current window and then loops 
-through them to get their title and url. It then formats the date for the filename and saves 
-it with formatting and date in filename using browser.downloads.download(). Finally, it adds 
-a listener for browser action click which calls saveTabs() when clicked.
-*/
-function saveTabs() {
+
+document.getElementById("t2j").addEventListener("click", function () {
   // get all tabs in the current window
   browser.tabs.query({ currentWindow: true }).then((tabs) => {
     let tabData = [];
@@ -17,9 +12,8 @@ function saveTabs() {
     }
     // format date for filename
     let date = new Date();
-    let dateString = `${date.getFullYear()}-${
-      date.getMonth() + 1
-    }-${date.getDate()}`;
+    let dateString = `${date.getFullYear()}-${date.getMonth() + 1
+      }-${date.getDate()}`;
 
     // save data to JSON file with formatting and date in filename
     let blob = new Blob([JSON.stringify(tabData, null, 2)], {
@@ -31,7 +25,23 @@ function saveTabs() {
       filename: `${dateString}-tabs.json`,
     });
   });
-}
+});
 
-// add listener for browser action click
-browser.browserAction.onClicked.addListener(saveTabs);
+/* Creating a listener for the button with the id of j2t. When the button is clicked, it will take the
+value of the textarea with the id of tabsJSON and parse it into an array of objects. Then it will
+loop through the array and create a new tab for each object. */
+document.getElementById("j2t").addEventListener("click", function () {
+
+  /* Getting the value of the textarea with the id of tabsJSON and parsing it into an array of objects. */
+  const tabsJ = document.getElementById("jsonTabs").value;
+  const parsedTabs = JSON.parse(tabsJ);
+
+  /* Looping through the array of objects and creating a new tab for each object. */
+  for (let tab of parsedTabs) {
+    console.log(tab.url);
+    browser.tabs.create({ url: tab.url });
+  }
+
+  window.close();
+});
+
